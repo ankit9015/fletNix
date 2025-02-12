66e88af8893469d7f587fe099a37ce4fca995dd6
Ankit Joshi <ankitoshi9015@gmail.com>
2025-02-13 00:44:15 +0530

    feat (frontend): add error page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ShowListComponent } from './components/show-list/show-list.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', title: 'Login', component: LoginComponent },
-  { path: 'register', title: 'Register', component: RegisterComponent },
+    { path: 'login', title: 'Login', component: LoginComponent },
+    { path: 'register', title: 'Register', component: RegisterComponent },
+    { path: 'shows', component: ShowListComponent, canActivate: [authGuard] },
+
+    { path: '', redirectTo: '/shows', pathMatch: 'full' },
+    { path: 'error', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/error' },
 ];
